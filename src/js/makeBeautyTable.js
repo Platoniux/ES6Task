468bb5114751dae761024d397c7aac7d0417b6dd
platon <chizzza@gmail.com>
2019-02-16 18:33:23 +0200

    first final version

--- src/js/makeBeautyTable.js
+++ src/js/makeBeautyTable.js
@@ -1,7 +1,6 @@
 ;(function () {
     'use strict';
 
-
     let classes = [
         'js-image--one',
         'js-image--two',
@@ -9,19 +8,24 @@
         'js-image--four'
     ];
 
-    const table = document.querySelector('.js-table');
-    const rowsAndCells = [
-        table.rows[0].cells,
-        table.rows[1].cells,
-        table.rows[2].cells,
-        table.rows[3].cells,
-        table.rows[4].cells,
-        table.rows[5].cells,
-        table.rows[6].cells,
-        table.rows[7].cells
-    ];
+    const tableForHighlight = document.querySelector('.js-table');
+    const rowsAndCells = [].map.call(tableForHighlight.rows, item => {
+       return item.cells;
+    });
 
-    const drawTable = (arrOfRows, arrOfClasses) => {
+    document.addEventListener('DOMContentLoaded',  () => {
+       drawTable(rowsAndCells, classes);
+    });
+
+    tableForHighlight.addEventListener('mouseover', e => {
+       makeMagic(e);
+    });
+
+    tableForHighlight.addEventListener('mouseout', () => {
+        document.querySelectorAll('.js-hover').forEach(element => element.classList.remove('js-hover'));
+    });
+
+    function drawTable(arrOfRows, arrOfClasses) {
         arrOfRows.forEach(arrOfCells => {
             [].map.call(arrOfCells, item => {
                 let randomNum = Math.round(Math.random() * 10);
@@ -31,54 +35,56 @@
                     if (randomNum < arrOfClasses.length) {
                         item.classList.add(arrOfClasses[randomNum]);
                     } else if ((randomNum - arrOfClasses.length) >= arrOfClasses.length) {
-                        item.classList.add(arrOfClasses[Math.round(randomNum / 3)]);
+                        item.classList.add(arrOfClasses[Math.round(randomNum / 19)]);
                     } else {
                         item.classList.add(arrOfClasses[randomNum - arrOfClasses.length]);
                     }
                 }
             });
         });
-    };
+    }
 
-    const checkCells = cell => {
+    function checkNextCells(cell) {
         let cellClass = cell.classList[1];
-        let prevCell = cell.previousElementSibling;
+        let indexOfCell = cell.cellIndex;
         let nextCell = cell.nextElementSibling;
-        if (prevCell) {
-            if (prevCell.classList.contains(cellClass)) {
-                prevCell.classList.add('js-hover');
-                checkCells(prevCell);
-            }
+        let prevCell = cell.previousElementSibling;
+        let prevRow = cell.parentElement.previousElementSibling;
+        let nextRow = cell.parentElement.nextElementSibling;
+        while (nextCell && nextCell.classList.contains(cellClass) && !nextCell.classList.contains('js-hover')) {
+            nextCell.classList.add('js-hover');
+            nextCell = nextCell.nextElementSibling;
         }
-        if (nextCell) {
-            if (nextCell.classList.contains(cellClass)) {
-                nextCell.classList.add('js-hover');
-                checkCells(nextCell);
-            }
+        while (prevCell && prevCell.classList.contains(cellClass) && !prevCell.classList.contains('js-hover')) {
+            prevCell.classList.add('js-hover');
+            prevCell = prevCell.previousElementSibling;
         }
-    };
+        while (nextRow && nextRow.cells[indexOfCell].classList.contains(cellClass) && !nextRow.cells[indexOfCell].classList.contains('js-hover')) {
+            nextRow.cells[indexOfCell].classList.add('js-hover');
+            nextRow = nextRow.nextElementSibling;
+        }
+        while (prevRow && prevRow.cells[indexOfCell].classList.contains(cellClass)) {
+            prevRow.cells[indexOfCell].classList.add('js-hover');
+            prevRow = prevRow.previousElementSibling;
+        }
+    }
 
-    const checkRows = event => {
-      let target = event.target;
-      let indexOfTarget = target.parentElement;
-    };
+    function workOnHover(arrOfRows) {
+        [].forEach.call(arrOfRows, cells => {
+           [].forEach.call(cells, item => {
+              if (item.classList.contains('js-hover')) {
+                  checkNextCells(item);
+              }
+           });
+        });
+    }
 
-    const makeMagic = event => {
+    function makeMagic(event) {
         let target = event.target;
         if (target.tagName === 'TD') {
-            checkCells(target);
+            target.classList.add('js-hover');
+            checkNextCells(target);
+            workOnHover(rowsAndCells);
         }
-    };
-
-    document.addEventListener('DOMContentLoaded',  () => {
-       drawTable(rowsAndCells, classes);
-    });
-
-    table.addEventListener('mouseover', e => {
-       makeMagic(e);
-    });
-
-    table.addEventListener('mouseout', () => {
-        document.querySelectorAll('.js-hover').forEach(element => element.classList.remove('js-hover'));
-    });
+    }
 }());
